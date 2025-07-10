840644e2bd0340100adcd0d261236c6d6661f0cb
sebastianvasquezrico156 <vasquezsebitas04@gmail.com>
2025-07-10 10:09:22 -0500

    Update ConexionBD.java

--- src/modelo/conexion/ConexionBD.java
+++ src/modelo/conexion/ConexionBD.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/veterinaria";
     private static final String USER = "root";
-    private static final String PASSWORD = "1096670931";
+    private static final String PASSWORD = "";
 
     public static Connection obtenerConexion() {
         try {
